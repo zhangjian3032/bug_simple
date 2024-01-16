29c684323c7ceff21fb26339bc669dab1b63a7ac
Jian Zhang <zhangjian.3032@bytedance.com>
2024-01-16 17:30:00 +0800

    add match with asio bug
    Signed-off-by: Jian Zhang <zhangjian.3032@bytedance.com>

---
+++ match/fake-client.cpp
@@ -0,0 +1,63 @@
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <sdbusplus/bus/match.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <variant>
+
+std::shared_ptr<sdbusplus::asio::dbus_interface> fooIfc;
+static std::string fooProperty = "foo";
+boost::asio::io_context io;
+auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+void syncCall()
+{
+    constexpr auto service = "com.foo";
+    constexpr auto path = "/com/foo";
+    constexpr auto interface = "com.foo";
+    constexpr auto property = "Foo";
+    std::variant<std::string> msg;
+
+    auto& bus = *conn;
+    auto method = bus.new_method_call(service, path,
+                                      "org.freedesktop.DBus.Properties", "Get");
+    method.append(interface, property);
+    auto reply = bus.call(method);
+    reply.read(msg);
+
+    auto now = std::chrono::system_clock::now();
+    lg2::info("{NOW} Sync Get Foo property to {FOO}", "NOW",
+              std::chrono::system_clock::to_time_t(now), "FOO",
+              std::get<std::string>(msg));
+    // if fail, throw exception, don't catch, will exit
+}
+
+void syncTimer()
+{
+    static boost::asio::steady_timer timer(io);
+    timer.expires_after(std::chrono::milliseconds(10));
+    timer.async_wait([](const boost::system::error_code& ec) {
+        if (ec)
+        {
+            return;
+        }
+        syncCall();
+        syncTimer();
+    });
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    syncTimer();
+
+    io.run();
+
+    return 0;
+}
